a986b1aed02354608c26dd0c3f36bf1a07e8a6be
Poinaa_bb <olegbarannikov72@yandex.ru>
2024-05-03 10:59:34 +0500

    review

---
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -0,0 +1,122 @@
+import { ArrowButton } from 'components/arrow-button';
+import { Button } from 'components/button';
+import {
+	ArticleStateType,
+	defaultArticleState,
+	fontColors,
+	fontSizeOptions,
+	fontFamilyOptions,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+
+import styles from './ArticleParamsForm.module.scss';
+
+import { useState, FormEvent } from 'react';
+import clsx from 'clsx';
+
+import { OpenCloseForm } from '../openCloseForm/OpenCloseForm';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+
+type ArticleParamsFormProps = {
+	params: ArticleStateType;
+	onChange: (params: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	params,
+	onChange,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const [fontFamilyOption, setFontFamilyOption] = useState(
+		params.fontFamilyOption
+	);
+
+	const [fontSizeOption, setFontSizeOption] = useState(params.fontSizeOption);
+
+	const [fontColor, setFontColor] = useState(params.fontColor);
+
+	const [backgroundColor, setBackground] = useState(params.backgroundColor);
+
+	const [contentWidth, setContentWidth] = useState(params.contentWidth);
+
+	const handleArrowButtonClick = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const handleSubmitForm = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		onChange({
+			fontFamilyOption,
+			fontSizeOption,
+			fontColor,
+			backgroundColor,
+			contentWidth,
+		});
+	};
+
+	const handleResetForm = () => {
+		setFontFamilyOption(defaultArticleState.fontFamilyOption);
+		setFontSizeOption(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackground(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+		onChange(defaultArticleState);
+	};
+
+	return (
+		<OpenCloseForm isOpen={isOpen} setIsOpen={setIsOpen}>
+			<ArrowButton isOpen={isOpen} onClick={handleArrowButtonClick} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmitForm}
+					onReset={handleResetForm}>
+					<h2 className={styles.title}>Задайте параметры</h2>
+					<Select
+						selected={fontFamilyOption}
+						options={fontFamilyOptions}
+						title={'Шрифт'}
+						onChange={setFontFamilyOption}
+					/>
+					<RadioGroup
+						name={'fontSize'}
+						selected={fontSizeOption}
+						options={fontSizeOptions}
+						title={'Размер шрифта'}
+						onChange={setFontSizeOption}
+					/>
+					<Select
+						selected={fontColor}
+						options={fontColors}
+						title={'Цвет шрифта'}
+						onChange={setFontColor}
+					/>
+					<Separator />
+					<Select
+						selected={backgroundColor}
+						options={backgroundColors}
+						title={'Цвет фона'}
+						onChange={setBackground}
+					/>
+					<Select
+						selected={contentWidth}
+						options={contentWidthArr}
+						title={'Ширина контента'}
+						onChange={setContentWidth}
+					/>
+					<div className={styles.bottomContainer}>
+						<Button title='Сбросить' type='reset' />
+						<Button title='Применить' type='submit' />
+					</div>
+				</form>
+			</aside>
+		</OpenCloseForm>
+	);
+};
---
+++ src/components/openCloseForm/index.tsx
@@ -0,0 +1,1 @@
+export { OpenCloseForm } from './OpenCloseForm';
