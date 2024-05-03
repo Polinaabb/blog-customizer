ec2b9d70cb78d25d927e0344f5e26a31c6435dbc
Poinaa_bb <olegbarannikov72@yandex.ru>
2024-05-03 17:51:34 +0500

    review3

--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState, useRef } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
-
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import { StrictMode } from 'react';
+import { App } from './components/app/App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const ref = useRef({
-		fontFamilyOption: defaultArticleState.fontFamilyOption,
-		fontColor: defaultArticleState.fontColor,
-		backgroundColor: defaultArticleState.backgroundColor,
-		contentWidth: defaultArticleState.contentWidth,
-		fontSizeOption: defaultArticleState.fontSizeOption,
-	});
-
-	const [state, setState] = useState(ref.current);
-
-	function handleChange(value: ArticleStateType) {
-		ref.current = {
-			fontFamilyOption: value.fontFamilyOption,
-			fontColor: value.fontColor,
-			backgroundColor: value.backgroundColor,
-			contentWidth: value.contentWidth,
-			fontSizeOption: value.fontSizeOption,
-		};
-		setState(ref.current);
-	}
-
-	return (
-		<div
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': state.fontFamilyOption.value,
-					'--font-size': state.fontSizeOption.value,
-					'--font-color': state.fontColor.value,
-					'--container-width': state.contentWidth.value,
-					'--bg-color': state.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm params={ref.current} onChange={handleChange} />
-			<Article />
-		</div>
-	);
-};
 
 root.render(
 	<StrictMode>
